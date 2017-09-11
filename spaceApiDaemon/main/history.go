0a2a11275db3c17db6d487ac871c20e6010f17bd
gidsi <gidsi@chaospott.de>
2017-09-11 18:53:16 +0200

    spaceApiDaemon: sort history

--- spaceApiDaemon/main/history.go
+++ spaceApiDaemon/main/history.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gidsi/SpaceApiSpec/v013"
 	"encoding/json"
 	"strings"
+	"sort"
 )
 
 var historyRoutes = routes{
@@ -28,10 +29,23 @@ type itemsSpaceAPIWithTimestamp struct {
 	Items []spaceapi_spec.State `json:"items"`
 }
 
+type StateList []spaceapi_spec.State
+
+func (s StateList) Len() int {
+	return len(s)
+}
+func (s StateList) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s StateList) Less(i, j int) bool {
+	return s[i].Lastchange < s[j].Lastchange
+}
+
+
 func getHistoryState(w http.ResponseWriter, _ *http.Request) {
 	spaceData, err := readSpaceData()
 
-	stateSlice := []spaceapi_spec.State{}
+	stateSlice := StateList{}
 	filteredStates := map[int]spaceapi_spec.State{}
 
 	if err == nil {
@@ -44,6 +58,8 @@ func getHistoryState(w http.ResponseWriter, _ *http.Request) {
 		for i := range filteredStates {
 			stateSlice = append(stateSlice, filteredStates[i])
 		}
+
+		sort.Sort(stateSlice)
 
 		returnJSON(w, itemsSpaceAPIWithTimestamp{stateSlice})
 	}
